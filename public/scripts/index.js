bdefdfb4f61d7f784335c2915cdd39aa244a8517
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-08-09 22:46:23 -0300

    [FEAT] graficos feitos

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,29 @@
+const ctx = document.getElementById("chart");
+
+let mainChart = new Chart(ctx,  {
+  type: 'bar',
+  data: {
+    labels: [],
+      datasets: [{
+        label: 'Informações sobre sua receita',
+        data: [],
+        borderWidth: 1,
+        backgroundColor: [
+          '#218837',
+          '#ce2d05',
+          '#F2AB27'
+        ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
 async function registra(event) {
   const valorBttn = event.submitter.value;
   let categoria;
@@ -18,10 +44,9 @@ async function registra(event) {
 }
 
 async function obterPlanilhas() {
-  const query = "SELECT * FROM registro_financeiro";
+  const query = "SELECT * FROM registro_financeiro ORDER BY data DESC";
   const method = "GET";
   const result = await consultaBanco(query, method);
-  console.log(result);
   createSpreadSheet(result)
 }
 
@@ -30,7 +55,6 @@ function createSpreadSheet(objets) {
   listCard.innerHTML = "";
 
   for (const obj of objets) {
-    console.log("OBJETO ==> "+ JSON.stringify(obj));
     const data = converterData(obj.data);
 
     const classColor = obj.categoria.toLowerCase(); 
@@ -50,7 +74,28 @@ function createSpreadSheet(objets) {
   }
 }
 
+async function updateChart() {
+  const query = `
+    SELECT receita.total receita, despesa.total despesa, (receita.total - despesa.total) total 
+    FROM 
+      (SELECT COALESCE(SUM(valor), 0) AS total 
+      FROM registro_financeiro where categoria = "Despesa") AS despesa, 
+      (SELECT COALESCE(SUM(valor), 0) AS total 
+      FROM registro_financeiro WHERE categoria = "Receita") AS receita
+    `;
+  const method = "GET";
+  const result = await consultaBanco(query, method);
+  const labels = Object.keys(result[0]);
+  const data = Object.values(result[0]).map(val => Number.parseInt(val))
+  
+  mainChart.data.labels = labels;
+  mainChart.data.datasets[0].data = data; 
+  mainChart.update();
+}
+
+
 obterPlanilhas();
+updateChart();
 
 function converterData(data) {
   let newData = new Date(data).toLocaleDateString();
